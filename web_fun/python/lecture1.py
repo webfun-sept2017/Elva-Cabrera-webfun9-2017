451ac21d5ab2da922095e65390c1a52d0191eba3
Elva Cabrera <elvagcabrera@gmail.com>
2017-10-07 13:54:45 -0500

    deleted old files and moved them

---
+++ web_fun/python/lecture1.py
@@ -0,0 +1,12 @@
+print "Hello"
+
+name = "travis"
+name += "10"
+print name
+tree = {
+    "trunk_color":"brown",
+    "number_of_leaves": 300,
+    "height":50
+    }
+print dir(tree)
+print tree.keys()
